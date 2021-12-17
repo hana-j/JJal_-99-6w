f9acf25210c20eab2894efe22720c47b80805258
hana <aswjdgksk@gmail.con>
2021-12-17 18:37:24 +0900

    test

---
+++ basic/test/async.test.js
@@ -0,0 +1,29 @@
+const fetchProduct = require('../async');
+
+describe('Async', (done)=>{
+    it('async-done' , ()=>{
+       fetchProduct().then((item)=>{
+           expect(item).toEqual({item:'Milk', price:2000});
+           done();
+       });
+    });
+    it('async-return' , ()=>{
+        return fetchProduct().then((item)=>{
+            expect(item).toEqual({item:'Milk', price:2000});
+            
+        });
+     });
+     it('async-await' , async()=>{
+        const product = await fetchProduct();
+            expect(product).toEqual({item:'Milk', price:2000});
+     });
+     it('async-resolves' , ()=>{
+        return expect(fetchProduct()).resolves.toEqual({
+            item:'Milk',price:2000
+        });
+     });
+     it('async-reject', ()=>{
+         return expect(fetchProduct('error')).rejects.toBe('network error');
+     })
+})
+ 
